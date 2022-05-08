ff6190fc06af0d1fe4191577200a0c0900954915
Vladislav Aduenko <adyenko@gmail.com>
2022-05-09 02:48:28 +0300

    feat(Card): add component remaining

---
+++ src/components/RemainingCard/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+interface IStyles {
+  isOverBudget: boolean;
+}
+
+export const StyledRemainingCard = styled.div<IStyles>`
+  padding: 38px 20px;
+
+  border-radius: 10px;
+  background: ${({ isOverBudget }) =>
+    isOverBudget ? "rgb(255 0 0)" : "rgb(204 213 255)"};
+`;
+
+export const Title = styled.p<IStyles>`
+  font-weight: ${({ isOverBudget }) => (isOverBudget ? "700" : "500")};
+  font-size: 20px;
+  line-height: 24px;
+
+  color: ${({ isOverBudget }) =>
+    isOverBudget ? "rgb(255 255 255)" : "rgb(0 0 0)"};
+`;
