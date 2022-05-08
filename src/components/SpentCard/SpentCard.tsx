f3ebd8f00934f6d7c992fd38a0a1dca3606c7f30
Vladislav Aduenko <adyenko@gmail.com>
2022-05-09 02:51:03 +0300

    feat(card): add new component

---
+++ src/components/SpentCard/SpentCard.tsx
@@ -0,0 +1,20 @@
+import { useCurrencyContex } from "../../context/CurrencyContext/CurrencyContext";
+import { useExpensesContext } from "../../context/ExpenseContext/ExpenseContext";
+import { StyledSpentCard, Title } from "./styles";
+
+const SpentCard = () => {
+  const { currency } = useCurrencyContex();
+  const { expenses } = useExpensesContext();
+
+  const spent = expenses.reduce((sum, { cost }) => sum + cost, 0);
+  return (
+    <StyledSpentCard>
+      <Title>
+        Spent so far: {currency}
+        {spent}
+      </Title>
+    </StyledSpentCard>
+  );
+};
+
+export default SpentCard;
